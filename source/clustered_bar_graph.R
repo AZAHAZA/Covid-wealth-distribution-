2e843bf39c538029b25ce45f9c5774a73b17f533
mattschcs <chuns4@uw.edu>
2022-02-18 03:51:01 -0800

    trying to input the graph to the index.RMD

--- source/clustered_bar_graph.R
+++ source/clustered_bar_graph.R
@@ -2,13 +2,14 @@
 
 #tidyverse
 library("tidyverse")
-#setwd("~/Desktop/Info201Project/final-project-starter-AZAHAZA")
-wealth_race <- read_csv("data/dfa-race.csv")
+#setwd("~/Desktop/Info201Project/final-project-starter-AZAHAZA/source")
+
+wealth_race <- read_csv("../data/dfa-race.csv")
 
 wealth_race_dist <- wealth_race %>%
   filter(Date =='2018:Q1'| Date=='2018:Q2'| Date=='2018:Q4'| Date=='2019:Q1'| Date=='2019:Q2'| Date=='2019:Q3'| Date=='2019:Q4'| Date=='2020:Q1'| Date=='2020:Q2'| Date=='2020:Q3'| Date=='2020:Q4'| Date=='2021:Q1'| Date=='2021:Q2'| Date=='2021:Q3')
 
-ggplot(wealth_race_dist)+
+ chart <- ggplot(wealth_race_dist)+
 geom_col(mapping = aes(x = Date, y = Assets,fill = Category), position = "dodge") +
 
 labs(
