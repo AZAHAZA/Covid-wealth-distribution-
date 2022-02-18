ee0727ab12dd1f72ee140ef215a89789f9e92895
mattschcs <chuns4@uw.edu>
2022-02-18 03:15:26 -0800

    completed the rough draft of the cluster bar chart

---
+++ source/clustered bar graph.R
@@ -0,0 +1,19 @@
+#Bar chart for the PO2
+
+#tidyverse
+library("tidyverse")
+#setwd("~/Desktop/Info201Project/final-project-starter-AZAHAZA")
+wealth_race <- read_csv("data/dfa-race.csv")
+
+wealth_race_dist <- wealth_race %>%
+  filter(Date =='2018:Q1'| Date=='2018:Q2'| Date=='2018:Q4'| Date=='2019:Q1'| Date=='2019:Q2'| Date=='2019:Q3'| Date=='2019:Q4'| Date=='2020:Q1'| Date=='2020:Q2'| Date=='2020:Q3'| Date=='2020:Q4'| Date=='2021:Q1'| Date=='2021:Q2'| Date=='2021:Q3')
+
+ggplot(wealth_race_dist)+
+geom_col(mapping = aes(x = Date, y = Assets,fill = Category), position = "dodge") +
+
+labs(
+  title = "Income distribution among different race during pre-panedmic and pandemic", 
+  x = "Year", 
+  y = "Assets",
+)
+
