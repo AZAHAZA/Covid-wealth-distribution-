e8e9d98f1b9fdbd44583a06bebd88e8420f0fd90
JohnnyCarroll22 <jcarol17@uw.edu>
2022-03-08 19:48:28 -0800

    updated intro

--- source/tab_panel_intro.R
+++ source/tab_panel_intro.R
@@ -4,5 +4,13 @@ library(shiny)
 
 tab_panel_intro <-tabPanel(
     "Introduction",
-    p("This is the introduction.")
+    p("Economic inequality is one of the largest issues currently facing the United States and is at the root 
+      of many social issues. By looking into such inequality, we are able to examine people’s quality of life 
+      and predict determinants of the health and well-being of individuals and families.  Over the past two 
+      years, the Covid-19 pandemic has place increased strain on many families as businesses closed and people
+      got sick.  The intentions of our project were to determine the effect that Covid-19 had on inequality in 
+      the United States across various groups including race, level of education and level of income.  The data
+      used in this data set details individual income levels for different groups and was collected by the National
+      Opinion Research Center (NORC) at the University of Chicago.  The data is also sponsored by the Federal Reserve
+      Board in cooperation with the Department of the Treasury.")
 )
